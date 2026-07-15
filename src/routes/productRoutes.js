484710be93dabee3bea9ffd9ecc792497b3210b1
Ravipratihast7667000989 <ravipratihast69@gmail.com>
2026-07-15 19:08:30 +0530

    first commit

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import productupload from "../middleware/multer.js";
-import { createProduct ,getAllProducts, searchProduct,deleteProduct} from "../controllers/productController.js";
+import { createProduct ,deleteProduct,getAllProducts, searchProduct} from "../controllers/productController.js";
 import { deleteCart } from "../controllers/addToCartController.js";
 
 const router = express.Router();
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post("/create", productupload.single("image"), createProduct);
 router.get("/all", getAllProducts);
 router.get("/search",searchProduct);
-router.delete("/delete:productId",deleteCart)
+router.delete("/delete:id",deleteProduct);
+router.delete("/delete:productId",deleteCart);
+
 
 export default router;
